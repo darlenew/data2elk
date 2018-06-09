e4a6e3706367c9914d659f3f355a6ec343c50e6e
dawong <dawong@paloaltonetworks.com>
2018-06-08 20:49:02 -0700

    Install the latest 6.x

--- elk_config.py
+++ elk_config.py
@@ -6,6 +6,20 @@ import sys
 import subprocess
 from commands import *
 
+
+def execute(cmd):
+    """Execute shell command"""
+    task  = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+    sys.stdout.write(cmd + '\n')
+    while True:
+          out = task.stderr.read(1)
+          if out == '' and task.poll() != None:
+             break
+          if out != '':
+             sys.stdout.write(out)
+             sys.stdout.flush()
+
+
 def ConfigElk(ip):
     """ Check configuration For Java & ELK components on Linux VM
         
@@ -17,6 +31,12 @@ def ConfigElk(ip):
     logcheck = getoutput('/etc/init.d/logstash status')
     if not "Java(TM) SE Runtime Environment" in jcheck:
        JavaUpdateCheck()
+
+    # install public signing key
+    execute("wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -")
+    execute("sudo apt-get install apt-transport-https")
+    execute("echo \"deb https://artifacts.elastic.co/packages/6.x/apt stable main\" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list")
+
     if not "elasticsearch is running" in escheck:
        ElasticSearchInstall(ip)
     if not "kibana is running" in kicheck:
@@ -25,20 +45,17 @@ def ConfigElk(ip):
        LogstashInstall(ip)
 
 
+def ubuntu_install(package):
+    sys.stdout.write("installing {}\n".format(package))
+    cmd = 'sudo apt-get update;sudo apt-get -y install {package}'.format(package=package)
+    execute(cmd)
+
+
 def JavaUpdateCheck():
     """ Configure the required Java Version on the host """
 
     java_cmd = "sudo add-apt-repository -y ppa:webupd8team/java ; sudo apt-get update ; sudo apt-get -y install oracle-java8-installer"
-    task  = subprocess.Popen(java_cmd, shell=True, stderr=subprocess.PIPE)
-    print task
-    while True:
-          out = task.stderr.read(1)
-          if out == '' and task.poll() != None:
-             break
-          if out != '':
-             sys.stdout.write(out)
-             sys.stdout.flush()
-     
+    execute(java_cmd)
    
 
 def ElasticSearchInstall(ip):
@@ -46,17 +63,9 @@ def ElasticSearchInstall(ip):
 
         @ip: IP address for Kibana & ElasticSearch Instance.
     """
-    elastic_search_cmd = 'wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -;echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list;sudo apt-get update;sudo apt-get -y install elasticsearch;sudo sed -i.bak "s/.*network.*host.*/network\.host: \"%s\"/g" /etc/elasticsearch/elasticsearch.yml;sudo service elasticsearch restart;sudo update-rc.d elasticsearch defaults 95 10'% ip
- 
-    task  = subprocess.Popen(elastic_search_cmd, shell=True, stderr=subprocess.PIPE)
-    print task
-    while True:
-          out = task.stderr.read(1)
-          if out == '' and task.poll() != None:
-             break
-          if out != '':
-             sys.stdout.write(out)
-             sys.stdout.flush()
+    ubuntu_install('elasticsearch')
+    elastic_search_cmd = 'sudo sed -i.bak "s/.*network.*host.*/network\.host: \"%s\"/g" /etc/elasticsearch/elasticsearch.yml;sudo service elasticsearch restart;sudo update-rc.d elasticsearch defaults 95 10'% ip
+    execute(elastic_search_cmd)
     
 
 def KibanaInstall(ip):
@@ -64,35 +73,19 @@ def KibanaInstall(ip):
 
         @ip: IP address for Kibana & ElasticSearch Instance.
     """
-
-    kibana_cmd = 'echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list;sudo apt-get update;sudo apt-get -y install kibana;sudo sed -i.bak "s/.*server\.host:.*/server\.host: \"%s\"/g" /opt/kibana/config/kibana.yml;sudo sed -i.bak "s/^# elasticsearch\.url/elasticsearch\.url/g" /opt/kibana/config/kibana.yml;sudo update-rc.d kibana defaults 96 9;sudo service kibana start'% ip
-
-    task  = subprocess.Popen(kibana_cmd, shell=True, stderr=subprocess.PIPE)
-    print task
-    while True:
-          out = task.stderr.read(1)
-          if out == '' and task.poll() != None:
-             break
-          if out != '':
-             sys.stdout.write(out)
-             sys.stdout.flush()
+    ubuntu_install('kibana')
+    kibana_cmd = 'sudo sed -i.bak "s/.*server\.host:.*/server\.host: \"%s\"/g" /opt/kibana/config/kibana.yml;sudo sed -i.bak "s/^# elasticsearch\.url/elasticsearch\.url/g" /opt/kibana/config/kibana.yml;sudo update-rc.d kibana defaults 96 9;sudo service kibana start'% ip
+    execute(kibana_cmd)
+ 
 
 def LogstashInstall(ip):
     """ Install & Configure Logstash on host
 
         @ip: IP address for Kibana & ElasticSearch Instance.
     """
+    ubuntu_install('logstash')
 
-    logstash_cmd = "echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash-2.2.x.list;sudo apt-get update;sudo apt-get install logstash"
-    task  = subprocess.Popen(logstash_cmd, shell=True, stderr=subprocess.PIPE)
-    print task
-    while True:
-          out = task.stderr.read(1)
-          if out == '' and task.poll() != None:
-             break
-          if out != '':
-             sys.stdout.write(out)
-             sys.stdout.flush()
+
 if __name__ == "__main__":
     ip = sys.argv[0]
     if len(sys.argv) > 1:
